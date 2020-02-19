db0c08f3a6da115ce2cc4c2ea6de9ce147ec5321
Sean Tisdale <setisdale@aggies.ncat.edu>
2020-02-18 19:53:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flashcards/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.flashcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
