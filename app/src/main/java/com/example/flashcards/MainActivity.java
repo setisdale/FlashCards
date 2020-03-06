b9c34b6d1ad0d52e8bc53a431c4c84aa6ba2ae99
setisdale <setisdale@aggies.ncat.edu>
2020-03-06 13:47:48 -0500

    App shows new data

--- app/src/main/java/com/example/flashcards/MainActivity.java
+++ app/src/main/java/com/example/flashcards/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -21,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
 
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
-                int i = 0;
             }
         });
 
@@ -37,8 +35,6 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.myBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            /*    Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                MainActivity.this.startActivity(intent);*/
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                 MainActivity.this.startActivityForResult(intent, 100);
             }
@@ -46,9 +42,16 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(data == null) {
+            return;
+        }
         if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
-            String s2 = data.getExtras().getString("textQuestion"); // 'string1' needs to match the key we used when we put the string in the Intent
-            String s1 = data.getExtras().getString("textAnswer");
+            String s2 = data.getExtras().getString("Question"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String s1 = data.getExtras().getString("Answer");
+            TextView flashcardquestion = findViewById(R.id.flashcard_question);
+            flashcardquestion.setText(s2);
+            TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
+            flashcardAnswer.setText(s1);
         }
     }
 
