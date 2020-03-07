8ab15600f3093a8161ab6788581e3a5b1fbf2bc1
setisdale <setisdale@aggies.ncat.edu>
2020-03-07 01:02:23 -0500

    App shows new data

--- app/src/main/java/com/example/flashcards/MainActivity.java
+++ app/src/main/java/com/example/flashcards/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int i;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
