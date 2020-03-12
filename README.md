0484b7358b2dcdfe608926c5026a8e7c580f59c1
setisdale <setisdale@aggies.ncat.edu>
2020-03-11 21:09:56 -0400

    Fixed some drawable files

---
+++ README.md
@@ -0,0 +1,32 @@
+# FlashCards
+# Lab 1
+Flashcard app that presents a question and when clicked shows a answer
+
+<img src="http://g.recordit.co/zDnk7By4F5.gif" width=200><br>
+
+
+## Required
+- [x] Create New Project in Android Studio
+- [x] Add a view for the front side of the flashcard to display the question
+- [x] Add a view for the back side of the flashcard to display the answer
+- [x] Build in logic to show the answer side when the card is tapped
+- [x] Push code to GitHu
+## Optional
+- [x] toggle the flashcard between the question side and the answer side
+
+
+## Lab 2
+
+### App Description
+App takes in data from user and presents new questions to the screen
+
+### App Walk-though
+<img src=http://g.recordit.co/yMipRJWc44.gif width=200><br>
+
+
+## Required
+- [x] User can click on a ‘+’ button that takes the user to new ‘Add Card Screen’
+- [x] The 'Add Card Screen' has a cancel button to take the user back to the main screen
+- [x] User can enter and submit a Question and Answer through the 'Add Card Screen'
+- [x] User will see the card they just created on the main screen when they press the 'Save Button'
+- [x] Push your progress to GitHub!
---
+++ app/src/main/java/com/example/flashcards/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.flashcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.myBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(data == null) {
+            return;
+        }
+        if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String s2 = data.getExtras().getString("Question"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String s1 = data.getExtras().getString("Answer");
+            TextView flashcardquestion = findViewById(R.id.flashcard_question);
+            flashcardquestion.setText(s2);
+            TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
+            flashcardAnswer.setText(s1);
+        }
+    }
+
+
+}
